6123342f9e84c263b7550bbf5aad6cb811ffd191
gerardpf2 <32612924+gerardpf2@users.noreply.github.com>
2025-03-10 16:14:09 +0100

    Added root, root composer and use case

---
+++ Assets/Scripts/Game/Root/UseCases/BuildAndInitializeRootScopeUseCase.cs
@@ -0,0 +1,95 @@
+using Game.Root.Composition;
+using Infrastructure.DependencyInjection;
+using Infrastructure.DependencyInjection.Rules;
+using Infrastructure.DependencyInjection.Utils;
+using JetBrains.Annotations;
+
+namespace Game.Root.UseCases
+{
+    // TODO: Test
+    public class BuildAndInitializeRootScopeUseCase : IBuildAndInitializeRootScopeUseCase
+    {
+        public Scope Resolve()
+        {
+            RuleContainer ruleContainer = new();
+            IRuleResolver ruleResolver = new RuleResolver(ruleContainer, null);
+
+            AddRules(ruleContainer);
+            Scope scope = Build(ruleResolver);
+            Initialize(ruleResolver, scope);
+
+            return scope;
+        }
+
+        private static void AddRules([NotNull] IRuleAdder ruleAdder)
+        {
+            ruleAdder.Add(new SingletonRule<IEnabledGateKeyGetter>(_ => new EnabledGateKeyContainer()));
+
+            ruleAdder.Add(
+                new SingletonRule<InjectResolver>(r =>
+                    new InjectResolver(
+                        r.Resolve<IRuleResolver>()
+                    )
+                )
+            );
+
+            ruleAdder.Add(new SingletonRule<RuleContainer>(_ => new RuleContainer()));
+            ruleAdder.Add(new ToRule<IRuleAdder, RuleContainer>());
+            ruleAdder.Add(new ToRule<IRuleGetter, RuleContainer>());
+
+            ruleAdder.Add(
+                new SingletonRule<IRuleFactory>(r =>
+                    new RuleFactory(
+                        r.Resolve<IEnabledGateKeyGetter>()
+                    )
+                )
+            );
+
+            ruleAdder.Add(
+                new SingletonRule<IRuleResolver>(r =>
+                    new RuleResolver(
+                        r.Resolve<IRuleGetter>(),
+                        null
+                    )
+                )
+            );
+
+            ruleAdder.Add(
+                new SingletonRule<IScopeBuilder>(r =>
+                    new ScopeBuilder(
+                        r.Resolve<IEnabledGateKeyGetter>(),
+                        r.Resolve<IScopeConstructor>(),
+                        r.Resolve<ISharedRuleAdder>(),
+                        r.Resolve<IRuleFactory>()
+                    )
+                )
+            );
+
+            ruleAdder.Add(new SingletonRule<IScopeComposer>(_ => new RootComposer()));
+
+            ruleAdder.Add(new SingletonRule<IScopeConstructor>(_ => new ScopeConstructor()));
+
+            ruleAdder.Add(new SingletonRule<IScopeInitializer>(_ => new ScopeInitializer()));
+
+            ruleAdder.Add(
+                new SingletonRule<ISharedRuleAdder>(r =>
+                    new SharedRuleAdder(
+                        r.Resolve<IRuleAdder>(),
+                        r.Resolve<IRuleFactory>()
+                    )
+                )
+            );
+        }
+
+        private static Scope Build([NotNull] IRuleResolver ruleResolver)
+        {
+            return ruleResolver.Resolve<IScopeBuilder>().BuildRoot(ruleResolver.Resolve<IScopeComposer>());
+        }
+
+        private static void Initialize([NotNull] IRuleResolver ruleResolver, Scope scope)
+        {
+            ruleResolver.Resolve<InjectResolver>();
+            ruleResolver.Resolve<IScopeInitializer>().Initialize(scope);
+        }
+    }
+}
