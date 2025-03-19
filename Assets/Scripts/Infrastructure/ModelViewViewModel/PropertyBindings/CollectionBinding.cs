7d3b262b9d09c916de5563f902f91a4c81312492
gerardpf2 <32612924+gerardpf2@users.noreply.github.com>
2025-03-19 15:11:52 +0100

    Updated collection binding with exception helpers

--- Assets/Scripts/Infrastructure/ModelViewViewModel/PropertyBindings/CollectionBinding.cs
+++ Assets/Scripts/Infrastructure/ModelViewViewModel/PropertyBindings/CollectionBinding.cs
@@ -1,6 +1,6 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
+using Infrastructure.System.Exceptions;
 using JetBrains.Annotations;
 using UnityEngine;
 
@@ -8,15 +8,22 @@ namespace Infrastructure.ModelViewViewModel.PropertyBindings
 {
     public abstract class CollectionBinding<T> : PropertyBinding<IEnumerable<T>>
     {
-        // TODO: Exceptions
-
         [SerializeField] private GameObject _prefab;
 
         [NotNull] private readonly IDictionary<T, GameObject> _instances = new Dictionary<T, GameObject>();
 
         public override void Set([ItemNotNull] IEnumerable<T> value)
         {
-            ICollection<T> currentData = new HashSet<T>(value ?? Enumerable.Empty<T>());
+            value ??= Enumerable.Empty<T>();
+
+            ICollection<T> currentData = new HashSet<T>();
+
+            foreach (T data in value)
+            {
+                ArgumentNullException.ThrowIfNull(data);
+
+                currentData.Add(data);
+            }
 
             RemoveObsoleteItems(currentData);
             AddItems(currentData);
@@ -24,6 +31,8 @@ namespace Infrastructure.ModelViewViewModel.PropertyBindings
 
         private void RemoveObsoleteItems([NotNull] ICollection<T> currentData)
         {
+            ArgumentNullException.ThrowIfNull(currentData);
+
             foreach (T data in _instances.Keys.ToList())
             {
                 if (currentData.Contains(data))
@@ -39,8 +48,12 @@ namespace Infrastructure.ModelViewViewModel.PropertyBindings
 
         private void AddItems([NotNull] [ItemNotNull] IEnumerable<T> currentData)
         {
+            ArgumentNullException.ThrowIfNull(currentData);
+
             foreach (T data in currentData)
             {
+                ArgumentNullException.ThrowIfNull(data);
+
                 if (!_instances.TryGetValue(data, out GameObject instance))
                 {
                     instance = Instantiate();
@@ -57,26 +70,24 @@ namespace Infrastructure.ModelViewViewModel.PropertyBindings
         [NotNull]
         private GameObject Instantiate()
         {
+            InvalidOperationException.ThrowIfNull(_prefab);
+
             GameObject instance = Instantiate(_prefab, transform);
 
-            if (!instance)
-            {
-                throw new InvalidOperationException("Cannot instantiate item");
-            }
+            InvalidOperationException.ThrowIfNull(instance);
 
             return instance;
         }
 
         private static void SetData([NotNull] GameObject instance, T data)
         {
-            if (instance.TryGetComponent(out IDataSettable<T> dataSettable))
-            {
-                dataSettable.SetData(data);
-            }
-            else
-            {
-                throw new InvalidOperationException($"Cannot set data of Type: {typeof(T)} to item");
-            }
+            ArgumentNullException.ThrowIfNull(instance);
+
+            IDataSettable<T> dataSettable = instance.GetComponent<IDataSettable<T>>();
+
+            InvalidOperationException.ThrowIfNull(dataSettable);
+
+            dataSettable.SetData(data);
         }
     }
 }
