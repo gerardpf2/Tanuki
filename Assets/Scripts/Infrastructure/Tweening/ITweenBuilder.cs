e93cad4d5361fcb51c7c3e3d107340d7cc86b12a
gerardpf2 <32612924+gerardpf2@users.noreply.github.com>
2025-03-23 20:55:31 +0100

    Added tween builder

---
+++ Assets/Scripts/Infrastructure/Tweening/ITweenBuilder.cs
@@ -0,0 +1,27 @@
+using System;
+using JetBrains.Annotations;
+using UnityEngine.UIElements;
+
+namespace Infrastructure.Tweening
+{
+    public interface ITweenBuilder<T>
+    {
+        [NotNull]
+        ITweenBuilder<T> WithStart(T start);
+
+        [NotNull]
+        ITweenBuilder<T> WithEnd(T end);
+
+        [NotNull]
+        ITweenBuilder<T> WithDurationS(float durationS);
+
+        [NotNull]
+        ITweenBuilder<T> WithSetter(Action<T> setter);
+
+        [NotNull]
+        ITweenBuilder<T> WithEasingMode(EasingMode easingMode);
+
+        [NotNull]
+        ITween Build();
+    }
+}
---
+++ Assets/Scripts/Infrastructure/Tweening/TweenBuilder.cs
@@ -0,0 +1,75 @@
+using System;
+using JetBrains.Annotations;
+using UnityEngine.UIElements;
+using ArgumentNullException = Infrastructure.System.Exceptions.ArgumentNullException;
+using InvalidOperationException = Infrastructure.System.Exceptions.InvalidOperationException;
+
+namespace Infrastructure.Tweening
+{
+    public abstract class TweenBuilder<T> : ITweenBuilder<T>
+    {
+        [NotNull] private readonly IEasingFunctionGetter _easingFunctionGetter;
+        [NotNull] private readonly Func<T, T, float, T> _lerp;
+
+        private T _start;
+        private T _end;
+        private float _durationS;
+        private Action<T> _setter;
+        private EasingMode _easingMode;
+
+        protected TweenBuilder(
+            [NotNull] IEasingFunctionGetter easingFunctionGetter,
+            [NotNull] Func<T, T, float, T> lerp)
+        {
+            ArgumentNullException.ThrowIfNull(easingFunctionGetter);
+            ArgumentNullException.ThrowIfNull(lerp);
+
+            _easingFunctionGetter = easingFunctionGetter;
+            _lerp = lerp;
+        }
+
+        public ITweenBuilder<T> WithStart(T start)
+        {
+            _start = start;
+
+            return this;
+        }
+
+        public ITweenBuilder<T> WithEnd(T end)
+        {
+            _end = end;
+
+            return this;
+        }
+
+        public ITweenBuilder<T> WithDurationS(float durationS)
+        {
+            _durationS = durationS;
+
+            return this;
+        }
+
+        public ITweenBuilder<T> WithSetter([NotNull] Action<T> setter)
+        {
+            ArgumentNullException.ThrowIfNull(setter);
+
+            _setter = setter;
+
+            return this;
+        }
+
+        public ITweenBuilder<T> WithEasingMode(EasingMode easingMode)
+        {
+            _easingMode = easingMode;
+
+            return this;
+        }
+
+        public ITween Build()
+        {
+            InvalidOperationException.ThrowIfNull(_setter);
+
+            return new Tween<T>(_start, _end, _durationS, _setter, _easingFunctionGetter.Get(_easingMode), _lerp);
+        }
+    }
+}
