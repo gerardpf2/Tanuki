328811098ab0be9bafe405fecdf2baab221d00ca
gerardpf2 <32612924+gerardpf2@users.noreply.github.com>
2025-03-31 12:48:03 +0200

    Updated argument out of range exception with throw if not

--- Assets/Scripts/Infrastructure/System/Exceptions/ArgumentOutOfRangeException.cs
+++ Assets/Scripts/Infrastructure/System/Exceptions/ArgumentOutOfRangeException.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Runtime.CompilerServices;
 using JetBrains.Annotations;
 
@@ -5,10 +6,22 @@ namespace Infrastructure.System.Exceptions
 {
     public static class ArgumentOutOfRangeException
     {
+        [NotNull] private static readonly Comparer Comparer = new();
+
         [ContractAnnotation("=> halt")]
         public static void Throw(object param, [CallerArgumentExpression("param")] string paramName = null)
         {
             throw new global::System.ArgumentOutOfRangeException(paramName, param, null);
+        }
+
+        public static void ThrowIfNot<T>([NotNull] T param, ComparisonOperator comparisonOperator, T value) where T : IComparable
+        {
+            ArgumentNullException.ThrowIfNull(param);
+
+            if (!Comparer.IsTrueThat(param, comparisonOperator, value))
+            {
+                Throw(param);
+            }
         }
     }
 }
