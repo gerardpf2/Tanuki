7e99e0e8edf639d92cd982d7a2e8264bdc99740a
gerardpf2 <32612924+gerardpf2@users.noreply.github.com>
2025-03-31 12:37:00 +0200

    Added is attribute

---
+++ Assets/Scripts/Infrastructure/System/IsAttribute.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Infrastructure.System
+{
+    // There is no behaviour behind this attribute for now, it is only used to decorate values to provide extra context
+
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Parameter | AttributeTargets.ReturnValue)]
+    public class IsAttribute : Attribute
+    {
+        public IsAttribute(ComparisonOperator comparisonOperator, int value) { }
+
+        public IsAttribute(ComparisonOperator comparisonOperator, float value) { }
+    }
+}
