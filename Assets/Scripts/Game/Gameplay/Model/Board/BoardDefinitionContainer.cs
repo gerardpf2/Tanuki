715fa23dd68d010534a58e4bd8d1e0dd40c4e477
gerardpf2 <32612924+gerardpf2@users.noreply.github.com>
2025-04-26 23:17:27 +0200

    Added board definition and board definition container

---
+++ Assets/Scripts/Game/Gameplay/Model/Board/BoardDefinitionContainer.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using Infrastructure.System.Exceptions;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace Game.Gameplay.Model.Board
+{
+    [CreateAssetMenu(fileName = nameof(BoardDefinitionContainer), menuName = "Tanuki/Game/Gameplay/Board/" + nameof(BoardDefinitionContainer))]
+    public class BoardDefinitionContainer : ScriptableObject, IBoardDefinitionGetter
+    {
+        [NotNull, ItemNotNull, SerializeField] private List<BoardDefinition> _boardDefinitions = new();
+
+        public IBoardDefinition Get(string id)
+        {
+            IBoardDefinition boardDefinition = _boardDefinitions.Find(boardDefinition => boardDefinition.Id == id);
+
+            InvalidOperationException.ThrowIfNullWithMessage(
+                boardDefinition,
+                $"Cannot get board definition with Id: {id}"
+            );
+
+            return boardDefinition;
+        }
+    }
+}
---
+++ Assets/Scripts/Game/Gameplay/Model/Board/IBoardDefinition.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using Infrastructure.System;
+using JetBrains.Annotations;
+
+namespace Game.Gameplay.Model.Board
+{
+    public interface IBoardDefinition
+    {
+        string Id { get; }
+
+        [Is(ComparisonOperator.GreaterThanOrEqualTo, 0)]
+        int Rows { get; }
+
+        [Is(ComparisonOperator.GreaterThanOrEqualTo, 0)]
+        int Columns { get; }
+
+        [NotNull, ItemNotNull]
+        IEnumerable<PiecePlacement> PiecePlacements { get; }
+    }
+}
