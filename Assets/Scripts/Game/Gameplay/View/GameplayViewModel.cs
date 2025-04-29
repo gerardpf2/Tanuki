840b14a5001dc1db6918a713e033ffdaf9745f19
gerardpf2 <32612924+gerardpf2@users.noreply.github.com>
2025-04-29 17:25:10 +0200

    Added data settable binding, board view data settable binding and updated prefab

--- Assets/Scripts/Game/Gameplay/View/GameplayViewModel.cs
+++ Assets/Scripts/Game/Gameplay/View/GameplayViewModel.cs
@@ -2,14 +2,11 @@ using Game.Gameplay.View.Board;
 using Infrastructure.ModelViewViewModel;
 using Infrastructure.System.Exceptions;
 using JetBrains.Annotations;
-using UnityEngine;
 
 namespace Game.Gameplay.View
 {
     public class GameplayViewModel : ViewModel, IDataSettable<GameplayViewData>
     {
-        [SerializeField] private BoardViewModel _boardViewModel; // TODO: Remove. Resolve using bindings
-
         [NotNull] private readonly IBoundProperty<BoardViewData> _boardViewData = new BoundProperty<BoardViewData>("BoardViewData", null);
 
         protected override void Awake()
@@ -22,8 +19,6 @@ namespace Game.Gameplay.View
         public void SetData([NotNull] GameplayViewData data)
         {
             ArgumentNullException.ThrowIfNull(data);
-
-            _boardViewModel.SetData(data.BoardViewData); // TODO: Remove. Resolve using bindings
 
             _boardViewData.Value = data.BoardViewData;
         }
