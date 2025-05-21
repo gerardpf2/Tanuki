3d08b9d10700120560f81abe47ff788bb4e324b1
gerardpf2 <32612924+gerardpf2@users.noreply.github.com>
2025-05-21 16:06:00 +0200

    Updated board definition container

--- Assets/Scripts/Game/Gameplay/Board/Parsing/PieceSerializedData.cs
+++ Assets/Scripts/Game/Gameplay/Board/Parsing/PieceSerializedData.cs
@@ -1,9 +1,12 @@
 using System.Collections.Generic;
+using Unity.Plastic.Newtonsoft.Json;
+using Unity.Plastic.Newtonsoft.Json.Converters;
 
 namespace Game.Gameplay.Board.Parsing
 {
     public class PieceSerializedData
     {
+        [JsonConverter(typeof(StringEnumConverter))]
         public PieceType PieceType { get; set; }
 
         public Dictionary<string, object> Metadata { get; set; }
