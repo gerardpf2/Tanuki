eeb43c8997516f6d7f0b2b397e1ec77650cf3afa
gerardpf2 <32612924+gerardpf2@users.noreply.github.com>
2025-06-09 12:47:11 +0200

    Updated goal view model and goal prefab with piece sprite

--- Assets/Scripts/Game/Gameplay/View/Header/Goals/GoalViewModel.cs
+++ Assets/Scripts/Game/Gameplay/View/Header/Goals/GoalViewModel.cs
@@ -1,13 +1,16 @@
+using Game.Gameplay.View.Common.Pieces;
 using Infrastructure.ModelViewViewModel;
 using Infrastructure.System.Exceptions;
 using JetBrains.Annotations;
+using UnityEngine;
 
 namespace Game.Gameplay.View.Header.Goals
 {
     public class GoalViewModel : ViewModel, IDataSettable<GoalViewData>
     {
-        // TODO: PieceType
+        [SerializeField] private PieceSpriteContainer _pieceSpriteContainer;
 
+        [NotNull] private readonly IBoundProperty<Sprite> _sprite = new BoundProperty<Sprite>("Sprite");
         [NotNull] private readonly IBoundProperty<string> _initialAmount = new BoundProperty<string>("InitialAmount");
         [NotNull] private readonly IBoundProperty<string> _currentAmount = new BoundProperty<string>("CurrentAmount");
 
@@ -15,6 +18,7 @@ namespace Game.Gameplay.View.Header.Goals
         {
             base.Awake();
 
+            Add(_sprite);
             Add(_initialAmount);
             Add(_currentAmount);
         }
@@ -22,7 +26,9 @@ namespace Game.Gameplay.View.Header.Goals
         public void SetData([NotNull] GoalViewData data)
         {
             ArgumentNullException.ThrowIfNull(data);
+            InvalidOperationException.ThrowIfNull(_pieceSpriteContainer);
 
+            _sprite.Value = _pieceSpriteContainer.Get(data.PieceType);
             _initialAmount.Value = data.InitialAmount.ToString();
             _currentAmount.Value = data.CurrentAmount.ToString();
         }
