4da40819b026309f5a6ad80e12a2e45ca9f4c6ce
gerardpf2 <32612924+gerardpf2@users.noreply.github.com>
2025-09-18 11:32:52 +0200

    Added game composer builder

---
+++ Assets/Scripts/Game/Composition/GameComposerBuilder.cs
@@ -0,0 +1,23 @@
+using Game.Gameplay;
+using Game.Gameplay.View.Board;
+using Infrastructure.DependencyInjection;
+using Infrastructure.System.Exceptions;
+using UnityEngine;
+
+namespace Game.Composition
+{
+    [CreateAssetMenu(fileName = nameof(GameComposerBuilder), menuName = "Tanuki/Game/Composition/" + nameof(GameComposerBuilder))]
+    public class GameComposerBuilder : GameScopeComposerBuilder
+    {
+        [SerializeField] private GameplayDefinitionContainer _gameplayDefinitionContainer;
+        [SerializeField] private PieceViewDefinitionContainer _pieceViewDefinitionContainer;
+
+        public override IScopeComposer Build()
+        {
+            InvalidOperationException.ThrowIfNull(_gameplayDefinitionContainer);
+            InvalidOperationException.ThrowIfNull(_pieceViewDefinitionContainer);
+
+            return new GameComposer(_gameplayDefinitionContainer, _pieceViewDefinitionContainer);
+        }
+    }
+}
