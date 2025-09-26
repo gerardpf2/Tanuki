3ff06408e9e747dfd14cbdd2682200851536ff46
gerardpf2 <32612924+gerardpf2@users.noreply.github.com>
2025-09-26 22:50:55 +0200

    Removed goal definition

--- Assets/Scripts/Game/Gameplay/IGameplayDefinition.cs
+++ Assets/Scripts/Game/Gameplay/IGameplayDefinition.cs
@@ -1,7 +1,3 @@
-using System.Collections.Generic;
-using Game.Gameplay.Goals;
-using JetBrains.Annotations;
-
 namespace Game.Gameplay
 {
     public interface IGameplayDefinition
@@ -11,8 +7,5 @@ namespace Game.Gameplay
         string Board { get; }
 
         string Goals { get; }
-
-        [NotNull, ItemNotNull]
-        IEnumerable<IGoalDefinition> GoalDefinitions { get; }
     }
 }
