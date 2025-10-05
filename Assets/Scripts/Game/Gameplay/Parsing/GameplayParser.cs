20f769b8365e41d87bb1155c6c8ef33e8814da4c
gerardpf2 <32612924+gerardpf2@users.noreply.github.com>
2025-10-05 15:56:29 +0200

    Added parser

---
+++ Assets/Scripts/Game/Gameplay/Parsing/GameplayParser.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using Game.Gameplay.Board;
+using Game.Gameplay.Goals;
+using Infrastructure.System.Exceptions;
+using Infrastructure.System.Parsing;
+using JetBrains.Annotations;
+
+namespace Game.Gameplay.Parsing
+{
+    public class GameplayParser : IGameplayParser
+    {
+        [NotNull] private readonly IGameplaySerializedDataConverter _gameplaySerializedDataConverter;
+        [NotNull] private readonly IParser _parser;
+
+        public GameplayParser(
+            [NotNull] IGameplaySerializedDataConverter gameplaySerializedDataConverter,
+            [NotNull] IParser parser)
+        {
+            ArgumentNullException.ThrowIfNull(gameplaySerializedDataConverter);
+            ArgumentNullException.ThrowIfNull(parser);
+
+            _gameplaySerializedDataConverter = gameplaySerializedDataConverter;
+            _parser = parser;
+        }
+
+        public string Serialize(IBoard board, IGoals goals)
+        {
+            GameplaySerializedData gameplaySerializedData = _gameplaySerializedDataConverter.From(board, goals);
+
+            return _parser.Serialize(gameplaySerializedData);
+        }
+
+        public void Deserialize(
+            string value,
+            out IBoard board,
+            out IEnumerable<PiecePlacement> piecePlacements,
+            out IGoals goals)
+        {
+            GameplaySerializedData gameplaySerializedData = _parser.Deserialize<GameplaySerializedData>(value);
+
+            _gameplaySerializedDataConverter.To(gameplaySerializedData, out board, out piecePlacements, out goals);
+        }
+    }
+}
