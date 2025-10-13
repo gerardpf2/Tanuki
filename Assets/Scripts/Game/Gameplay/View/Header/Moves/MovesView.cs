c02988d25e099db0c55680ef79e722140257ae22
gerardpf2 <32612924+gerardpf2@users.noreply.github.com>
2025-10-13 11:45:40 +0200

    Added moves view

---
+++ Assets/Scripts/Game/Gameplay/View/Header/Moves/MovesView.cs
@@ -0,0 +1,68 @@
+using System;
+using Game.Common;
+using Game.Gameplay.Moves;
+using JetBrains.Annotations;
+using ArgumentNullException = Infrastructure.System.Exceptions.ArgumentNullException;
+using InvalidOperationException = Infrastructure.System.Exceptions.InvalidOperationException;
+
+namespace Game.Gameplay.View.Header.Moves
+{
+    public class MovesView : IMovesView
+    {
+        [NotNull] private readonly IMovesContainer _movesContainer;
+
+        private InitializedLabel _initializedLabel;
+
+        private IMoves _moves;
+
+        public event Action OnUpdated;
+
+        public int Amount
+        {
+            get
+            {
+                InvalidOperationException.ThrowIfNull(_moves);
+
+                return _moves.Amount;
+            }
+            set
+            {
+                InvalidOperationException.ThrowIfNull(_moves);
+
+                if (Amount == value)
+                {
+                    return;
+                }
+
+                _moves.Amount = value;
+
+                OnUpdated?.Invoke();
+            }
+        }
+
+        public MovesView([NotNull] IMovesContainer movesContainer)
+        {
+            ArgumentNullException.ThrowIfNull(movesContainer);
+
+            _movesContainer = movesContainer;
+        }
+
+        public void Initialize()
+        {
+            _initializedLabel.SetInitialized();
+
+            IMoves moves = _movesContainer.Moves;
+
+            InvalidOperationException.ThrowIfNull(moves);
+
+            _moves = moves; // TODO: Clone
+        }
+
+        public void Uninitialize()
+        {
+            _initializedLabel.SetUninitialized();
+
+            _moves = null;
+        }
+    }
+}
