058fba58086435373279a2f12f33531500bacf9e
gerardpf2 <32612924+gerardpf2@users.noreply.github.com>
2025-10-21 12:51:46 +0200

    Movement helper added do camera movement

--- Assets/Scripts/Game/Gameplay/View/Animation/Movement/IMovementHelper.cs
+++ Assets/Scripts/Game/Gameplay/View/Animation/Movement/IMovementHelper.cs
@@ -6,5 +6,7 @@ namespace Game.Gameplay.View.Animation.Movement
     public interface IMovementHelper
     {
         void DoGravityMovement(Transform transform, int rowOffset, int columnOffset, Action onComplete);
+
+        void DoCameraMovement(Transform transform, int rowOffset, Action onComplete);
     }
 }
