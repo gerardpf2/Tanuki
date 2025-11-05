ae29e760bc572a2233bdfbba70532084775013d9
gerardpf2 <32612924+gerardpf2@users.noreply.github.com>
2025-11-05 19:22:03 +0100

    Added base piece view model and updated piece view model

--- Assets/Scripts/Game/Gameplay/View/Pieces/Pieces/PieceViewModel.cs
+++ Assets/Scripts/Game/Gameplay/View/Pieces/Pieces/PieceViewModel.cs
@@ -1,45 +1,13 @@
 using System;
 using Game.Gameplay.Events.Reasons;
 using Game.Gameplay.Pieces.Pieces;
-using Infrastructure.ModelViewViewModel;
-using Infrastructure.Unity.Utils;
-using UnityEngine;
-using ArgumentException = Infrastructure.System.Exceptions.ArgumentException;
-using InvalidOperationException = Infrastructure.System.Exceptions.InvalidOperationException;
 
 namespace Game.Gameplay.View.Pieces.Pieces
 {
     public class PieceViewModel : PieceViewModel<IPiece> { }
 
-    public abstract class PieceViewModel<T> : ViewModel, IDataSettable<IPiece>, IPieceViewEventNotifier where T : IPiece
+    public abstract class PieceViewModel<T> : BasePieceViewModel<T>, IBoardPieceViewEventNotifier where T : IPiece
     {
-        [SerializeField] private Transform _content;
-
-        protected T Piece;
-
-        public void SetData(IPiece data)
-        {
-            ArgumentException.ThrowIfTypeIsNot<T>(data);
-
-            Piece = (T)data;
-
-            SyncState();
-        }
-
-        public void OnInstantiated(InstantiatePieceReason instantiatePieceReason, Action onComplete)
-        {
-            // TODO
-
-            onComplete?.Invoke();
-        }
-
-        public void OnDestroyed(DestroyPieceReason destroyPieceReason, Action onComplete)
-        {
-            // TODO
-
-            onComplete?.Invoke();
-        }
-
         public void OnDamaged(DamagePieceReason damagePieceReason, Action onComplete)
         {
             // TODO
@@ -59,25 +27,6 @@ namespace Game.Gameplay.View.Pieces.Pieces
             // TODO
 
             onComplete?.Invoke();
-        }
-
-        public void OnRotated()
-        {
-            SyncRotation();
-        }
-
-        protected virtual void SyncState()
-        {
-            SyncRotation();
-        }
-
-        private void SyncRotation()
-        {
-            InvalidOperationException.ThrowIfNull(_content);
-            InvalidOperationException.ThrowIfNull(Piece);
-
-            _content.localPosition = _content.localPosition.WithX(0.5f * (Piece.Width - 1)).WithY(0.5f * Piece.Height);
-            _content.localRotation = Quaternion.Euler(0.0f, 0.0f, -90.0f * Piece.Rotation); // Clockwise rotation
         }
     }
 }
