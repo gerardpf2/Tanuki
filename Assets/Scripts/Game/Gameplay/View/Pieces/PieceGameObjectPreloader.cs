e55c557f148f3e97c683580f812175b1c86ef2ca
gerardpf2 <32612924+gerardpf2@users.noreply.github.com>
2025-11-05 11:34:40 +0100

    Updated preloader logic so it checks piece placements

--- Assets/Scripts/Game/Gameplay/View/Pieces/PieceGameObjectPreloader.cs
+++ Assets/Scripts/Game/Gameplay/View/Pieces/PieceGameObjectPreloader.cs
@@ -1,36 +1,79 @@
-using System;
+using System.Collections.Generic;
 using Game.Common.Pieces;
+using Game.Gameplay.Board;
+using Game.Gameplay.Pieces;
+using Infrastructure.System.Exceptions;
 using Infrastructure.Unity.Pooling;
 using JetBrains.Annotations;
-using ArgumentNullException = Infrastructure.System.Exceptions.ArgumentNullException;
 
 namespace Game.Gameplay.View.Pieces
 {
     public class PieceGameObjectPreloader : IPieceGameObjectPreloader
     {
+        [NotNull] private readonly IBoardContainer _boardContainer;
         [NotNull] private readonly IPieceViewDefinitionGetter _pieceViewDefinitionGetter;
         [NotNull] private readonly IGameObjectPool _gameObjectPool;
 
+        [NotNull] private readonly IDictionary<PieceType, int> _amountByPieceType = new Dictionary<PieceType, int>();
+
         public PieceGameObjectPreloader(
+            [NotNull] IBoardContainer boardContainer,
             [NotNull] IPieceViewDefinitionGetter pieceViewDefinitionGetter,
             [NotNull] IGameObjectPool gameObjectPool)
         {
+            ArgumentNullException.ThrowIfNull(boardContainer);
             ArgumentNullException.ThrowIfNull(pieceViewDefinitionGetter);
             ArgumentNullException.ThrowIfNull(gameObjectPool);
 
+            _boardContainer = boardContainer;
             _pieceViewDefinitionGetter = pieceViewDefinitionGetter;
             _gameObjectPool = gameObjectPool;
         }
 
         public void Preload()
         {
-            // TODO: Use board container piece placements and bag initial piece types and piece entries
+            _amountByPieceType.Clear();
 
-            const int amount = 5;
+            AddBoardAmounts();
+            AddBagAmounts();
+            PreloadImpl();
+        }
 
-            foreach (object pieceTypeObj in Enum.GetValues(typeof(PieceType)))
+        private void AddBoardAmounts()
+        {
+            IEnumerable<PiecePlacement> piecePlacements = _boardContainer.PiecePlacements;
+
+            InvalidOperationException.ThrowIfNull(piecePlacements);
+
+            foreach (PiecePlacement piecePlacement in piecePlacements)
             {
-                PieceType pieceType = (PieceType)pieceTypeObj;
+                InvalidOperationException.ThrowIfNull(piecePlacement);
+
+                IncreaseAmount(piecePlacement.Piece.Type);
+            }
+        }
+
+        private void AddBagAmounts()
+        {
+            // TODO: Remove if not needed
+        }
+
+        private void IncreaseAmount(PieceType pieceType)
+        {
+            if (_amountByPieceType.TryGetValue(pieceType, out int amount))
+            {
+                _amountByPieceType[pieceType] = amount + 1;
+            }
+            else
+            {
+                _amountByPieceType.Add(pieceType, 1);
+            }
+        }
+
+        private void PreloadImpl()
+        {
+            foreach ((PieceType pieceType, int amount) in _amountByPieceType)
+            {
                 IPieceViewDefinition pieceViewDefinition = _pieceViewDefinitionGetter.Get(pieceType);
 
                 _gameObjectPool.Preload(pieceViewDefinition.Prefab, amount, true);
