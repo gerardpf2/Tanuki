f1491679555a506f47af395d01866f9a10c94eee
gerardpf2 <32612924+gerardpf2@users.noreply.github.com>
2025-11-07 12:23:09 +0100

    Added normal and pressed sprite refs and logic and added image binding

--- Assets/Scripts/Game/Common/UI/ButtonViewModel.cs
+++ Assets/Scripts/Game/Common/UI/ButtonViewModel.cs
@@ -1,16 +1,25 @@
 using Infrastructure.ModelViewViewModel;
 using Infrastructure.System.Exceptions;
 using JetBrains.Annotations;
+using UnityEngine;
 
 namespace Game.Common.UI
 {
     public class ButtonViewModel : ViewModel, IDataSettable<ButtonViewData>
     {
+        [SerializeField] private Sprite _normalSprite;
+        [SerializeField] private Sprite _pressedSprite;
+
+        [NotNull] private readonly IBoundProperty<Sprite> _sprite = new BoundProperty<Sprite>("Sprite");
+
         private ButtonViewData _buttonViewData;
+        private bool _pressed;
 
         protected override void Awake()
         {
             base.Awake();
+
+            Add(_sprite);
 
             Add(new BoundMethod(OnPointerDown));
             Add(new BoundMethod(OnPointerUp));
@@ -21,11 +30,15 @@ namespace Game.Common.UI
             ArgumentNullException.ThrowIfNull(data);
 
             _buttonViewData = data;
+
+            RefreshSprite();
         }
 
         private void OnPointerDown()
         {
+            _pressed = true;
 
+            RefreshSprite();
         }
 
         private void OnPointerUp()
@@ -33,6 +46,15 @@ namespace Game.Common.UI
             InvalidOperationException.ThrowIfNull(_buttonViewData);
 
             _buttonViewData.OnClick?.Invoke();
+
+            _pressed = false;
+
+            RefreshSprite();
+        }
+
+        private void RefreshSprite()
+        {
+            _sprite.Value = _pressed ? _pressedSprite : _normalSprite;
         }
     }
 }
