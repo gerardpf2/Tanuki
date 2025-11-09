17c5227267b9c4e8f7a53b9665a360b6433edc51
gerardpf2 <32612924+gerardpf2@users.noreply.github.com>
2025-11-09 11:18:48 +0100

    Added base instantiate player piece phase and updated both instantiate and simulate instantiate player piece phase

--- Assets/Scripts/Game/Gameplay/Phases/Phases/InstantiatePlayerPiecePhase.cs
+++ Assets/Scripts/Game/Gameplay/Phases/Phases/InstantiatePlayerPiecePhase.cs
@@ -1,21 +1,15 @@
-using System;
 using Game.Gameplay.Bag;
 using Game.Gameplay.Board;
-using Game.Gameplay.Board.Utils;
 using Game.Gameplay.Camera;
 using Game.Gameplay.Events;
 using Game.Gameplay.Pieces.Pieces;
+using Infrastructure.System.Exceptions;
 using JetBrains.Annotations;
-using ArgumentNullException = Infrastructure.System.Exceptions.ArgumentNullException;
-using InvalidOperationException = Infrastructure.System.Exceptions.InvalidOperationException;
 
 namespace Game.Gameplay.Phases.Phases
 {
-    public class InstantiatePlayerPiecePhase : Phase
+    public class InstantiatePlayerPiecePhase : BaseInstantiatePlayerPiecePhase
     {
-        [NotNull] private readonly IBagContainer _bagContainer;
-        [NotNull] private readonly IBoardContainer _boardContainer;
-        [NotNull] private readonly ICamera _camera;
         [NotNull] private readonly IEventEnqueuer _eventEnqueuer;
         [NotNull] private readonly IEventFactory _eventFactory;
 
@@ -26,69 +20,28 @@ namespace Game.Gameplay.Phases.Phases
             [NotNull] IBoardContainer boardContainer,
             [NotNull] ICamera camera,
             [NotNull] IEventEnqueuer eventEnqueuer,
-            [NotNull] IEventFactory eventFactory)
+            [NotNull] IEventFactory eventFactory) : base(bagContainer, boardContainer, camera)
         {
-            ArgumentNullException.ThrowIfNull(bagContainer);
-            ArgumentNullException.ThrowIfNull(boardContainer);
-            ArgumentNullException.ThrowIfNull(camera);
             ArgumentNullException.ThrowIfNull(eventEnqueuer);
             ArgumentNullException.ThrowIfNull(eventFactory);
 
-            _bagContainer = bagContainer;
-            _boardContainer = boardContainer;
-            _camera = camera;
             _eventEnqueuer = eventEnqueuer;
             _eventFactory = eventFactory;
         }
 
-        protected override ResolveResult ResolveImpl([NotNull] ResolveContext resolveContext)
+        protected override ResolveResult ResolveImpl(
+            [NotNull] ResolveContext resolveContext,
+            IPiece piece,
+            Coordinate sourceCoordinate,
+            Coordinate lockSourceCoordinate)
         {
             ArgumentNullException.ThrowIfNull(resolveContext);
-
-            IPiece piece = GetPiece();
-            Coordinate sourceCoordinate = GetSourceCoordinate(piece);
-            Coordinate lockSourceCoordinate = GetLockSourceCoordinate(piece, sourceCoordinate);
 
             resolveContext.SetPieceSourceCoordinate(sourceCoordinate, lockSourceCoordinate);
 
             _eventEnqueuer.Enqueue(_eventFactory.GetInstantiatePlayerPieceEvent(piece, sourceCoordinate));
 
             return ResolveResult.Updated;
-        }
-
-        [NotNull]
-        private IPiece GetPiece()
-        {
-            IBag bag = _bagContainer.Bag;
-
-            InvalidOperationException.ThrowIfNull(bag);
-
-            return bag.Current;
-        }
-
-        private Coordinate GetSourceCoordinate([NotNull] IPiece piece)
-        {
-            ArgumentNullException.ThrowIfNull(piece);
-
-            IBoard board = _boardContainer.Board;
-
-            InvalidOperationException.ThrowIfNull(board);
-
-            int row = Math.Max(board.HighestNonEmptyRow + _camera.ExtraRowsOnTop, _camera.TopRow) - piece.Height + 1;
-            int column = (board.Columns - piece.Width + 1) / 2;
-
-            return new Coordinate(row, column);
-        }
-
-        private Coordinate GetLockSourceCoordinate([NotNull] IPiece piece, Coordinate sourceCoordinate)
-        {
-            ArgumentNullException.ThrowIfNull(piece);
-
-            IBoard board = _boardContainer.Board;
-
-            InvalidOperationException.ThrowIfNull(board);
-
-            return board.GetPieceLockSourceCoordinate(piece, sourceCoordinate);
         }
     }
 }
--- Assets/Scripts/Game/Gameplay/Phases/Phases/SimulateInstantiatePlayerPiecePhase.cs
+++ Assets/Scripts/Game/Gameplay/Phases/Phases/SimulateInstantiatePlayerPiecePhase.cs
@@ -1,36 +1,24 @@
-using System;
 using Game.Gameplay.Bag;
 using Game.Gameplay.Board;
-using Game.Gameplay.Board.Utils;
 using Game.Gameplay.Camera;
 using Game.Gameplay.Pieces.Pieces;
+using Infrastructure.System.Exceptions;
 using JetBrains.Annotations;
-using ArgumentNullException = Infrastructure.System.Exceptions.ArgumentNullException;
-using InvalidOperationException = Infrastructure.System.Exceptions.InvalidOperationException;
 
 namespace Game.Gameplay.Phases.Phases
 {
-    public class SimulateInstantiatePlayerPiecePhase : Phase
+    public class SimulateInstantiatePlayerPiecePhase : BaseInstantiatePlayerPiecePhase
     {
-        [NotNull] private readonly IBagContainer _bagContainer;
-        [NotNull] private readonly IBoardContainer _boardContainer;
-        [NotNull] private readonly ICamera _camera;
-
         public SimulateInstantiatePlayerPiecePhase(
             [NotNull] IBagContainer bagContainer,
             [NotNull] IBoardContainer boardContainer,
-            [NotNull] ICamera camera)
-        {
-            ArgumentNullException.ThrowIfNull(bagContainer);
-            ArgumentNullException.ThrowIfNull(boardContainer);
-            ArgumentNullException.ThrowIfNull(camera);
+            [NotNull] ICamera camera) : base(bagContainer, boardContainer, camera) { }
 
-            _bagContainer = bagContainer;
-            _boardContainer = boardContainer;
-            _camera = camera;
-        }
-
-        protected override ResolveResult ResolveImpl([NotNull] ResolveContext resolveContext)
+        protected override ResolveResult ResolveImpl(
+            [NotNull] ResolveContext resolveContext,
+            IPiece piece,
+            Coordinate sourceCoordinate,
+            Coordinate lockSourceCoordinate)
         {
             ArgumentNullException.ThrowIfNull(resolveContext);
 
@@ -42,54 +30,14 @@ namespace Game.Gameplay.Phases.Phases
             Coordinate prevSourceCoordinate = resolveContext.PieceSourceCoordinate.Value;
             Coordinate prevLockSourceCoordinate = resolveContext.PieceLockSourceCoordinate.Value;
 
-            IPiece piece = GetPiece();
-            Coordinate newSourceCoordinate = GetSourceCoordinate(piece);
-            Coordinate newLockSourceCoordinate = GetLockSourceCoordinate(piece, newSourceCoordinate);
-
-            if (newSourceCoordinate.Equals(prevSourceCoordinate) &&
-                newLockSourceCoordinate.Equals(prevLockSourceCoordinate))
+            if (sourceCoordinate.Equals(prevSourceCoordinate) && lockSourceCoordinate.Equals(prevLockSourceCoordinate))
             {
                 return ResolveResult.NotUpdated;
             }
 
-            resolveContext.SetPieceSourceCoordinate(newSourceCoordinate, newLockSourceCoordinate);
+            resolveContext.SetPieceSourceCoordinate(sourceCoordinate, lockSourceCoordinate);
 
             return ResolveResult.Updated;
-        }
-
-        [NotNull]
-        private IPiece GetPiece()
-        {
-            IBag bag = _bagContainer.Bag;
-
-            InvalidOperationException.ThrowIfNull(bag);
-
-            return bag.Current;
-        }
-
-        private Coordinate GetSourceCoordinate([NotNull] IPiece piece)
-        {
-            ArgumentNullException.ThrowIfNull(piece);
-
-            IBoard board = _boardContainer.Board;
-
-            InvalidOperationException.ThrowIfNull(board);
-
-            int row = Math.Max(board.HighestNonEmptyRow + _camera.ExtraRowsOnTop, _camera.TopRow) - piece.Height + 1;
-            int column = (board.Columns - piece.Width + 1) / 2;
-
-            return new Coordinate(row, column);
-        }
-
-        private Coordinate GetLockSourceCoordinate([NotNull] IPiece piece, Coordinate sourceCoordinate)
-        {
-            ArgumentNullException.ThrowIfNull(piece);
-
-            IBoard board = _boardContainer.Board;
-
-            InvalidOperationException.ThrowIfNull(board);
-
-            return board.GetPieceLockSourceCoordinate(piece, sourceCoordinate);
         }
     }
 }
