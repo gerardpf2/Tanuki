b0f78d68da95c330af999cb18cbe2a2a0925f60e
gerardpf2 <32612924+gerardpf2@users.noreply.github.com>
2025-11-13 11:07:27 +0100

    Added board view ground view model and updated prefab

--- Assets/Scripts/Game/Gameplay/View/Board/Composition/BoardComposer.cs
+++ Assets/Scripts/Game/Gameplay/View/Board/Composition/BoardComposer.cs
@@ -48,6 +48,14 @@ namespace Game.Gameplay.View.Board.Composition
                     )
                 )
             );
+
+            ruleAdder.Add(
+                ruleFactory.GetInject<BoardViewGroundViewModel>((r, vm) =>
+                    vm.Inject(
+                        r.Resolve<IBoard>("View")
+                    )
+                )
+            );
         }
     }
 }
