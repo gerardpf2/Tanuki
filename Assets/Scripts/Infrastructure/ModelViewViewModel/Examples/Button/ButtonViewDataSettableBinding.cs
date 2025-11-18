8414bcec84fcb897b56ee4d845719887d6c98604
gerardpf2 <32612924+gerardpf2@users.noreply.github.com>
2025-11-18 20:05:40 +0100

    Added button view model, view data and view data settable binding and its prefab

---
+++ Assets/Scripts/Infrastructure/ModelViewViewModel/Examples/Button/ButtonViewDataSettableBinding.cs
@@ -0,0 +1,6 @@
+using Infrastructure.ModelViewViewModel.PropertyBindings;
+
+namespace Infrastructure.ModelViewViewModel.Examples.Button
+{
+    public class ButtonViewDataSettableBinding : DataSettableBinding<ButtonViewData> { }
+}
