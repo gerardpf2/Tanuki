976b4b77c8521d697db74e20133d40bacc3e287a
gerardpf2 <32612924+gerardpf2@users.noreply.github.com>
2025-11-25 15:20:37 +0100

    Added base player pieces game object preloader

--- Assets/Scripts/Game/Gameplay/View/Pieces/Preloader/PlayerPieceGhostsGameObjectPreloader.cs
+++ Assets/Scripts/Game/Gameplay/View/Pieces/Preloader/PlayerPieceGhostsGameObjectPreloader.cs
@@ -1,52 +1,25 @@
-using System.Collections.Generic;
 using Game.Common.Pieces;
 using Game.Gameplay.Bag;
 using Infrastructure.System.Exceptions;
 using Infrastructure.Unity.Pooling;
 using JetBrains.Annotations;
-using UnityEngine;
 
 namespace Game.Gameplay.View.Pieces.Preloader
 {
-    public class PlayerPieceGhostsGameObjectPreloader : BasePieceGameObjectPreloader
+    public class PlayerPieceGhostsGameObjectPreloader : BasePlayerPiecesGameObjectPreloader
     {
-        [NotNull] private readonly IBag _bag;
-
         public PlayerPieceGhostsGameObjectPreloader(
             [NotNull] IPieceViewDefinitionGetter pieceViewDefinitionGetter,
             [NotNull] IGameObjectPool gameObjectPool,
-            [NotNull] IBag bag) : base(pieceViewDefinitionGetter, gameObjectPool)
-        {
-            ArgumentNullException.ThrowIfNull(bag);
+            [NotNull] IBag bag) : base(pieceViewDefinitionGetter, gameObjectPool, bag) { }
 
-            _bag = bag;
-        }
-
-        protected override IEnumerable<PreloadRequest> GetPreloadRequests(
-            [NotNull] IPieceViewDefinitionGetter pieceViewDefinitionGetter)
+        protected override IPieceViewDefinition GetPieceViewDefinition(
+            [NotNull] IPieceViewDefinitionGetter pieceViewDefinitionGetter,
+            PieceType pieceType)
         {
             ArgumentNullException.ThrowIfNull(pieceViewDefinitionGetter);
 
-            foreach (BagPieceEntry bagPieceEntry in _bag.BagPieceEntries)
-            {
-                yield return GetPreloadRequest(bagPieceEntry.PieceType);
-            }
-
-            foreach (PieceType pieceType in _bag.InitialPieceTypes)
-            {
-                yield return GetPreloadRequest(pieceType);
-            }
-
-            yield break;
-
-            PreloadRequest GetPreloadRequest(PieceType pieceType)
-            {
-                const int amount = 1;
-
-                GameObject prefab = pieceViewDefinitionGetter.GetPlayerPieceGhost(pieceType).Prefab;
-
-                return new PreloadRequest(prefab, amount);
-            }
+            return pieceViewDefinitionGetter.GetPlayerPieceGhost(pieceType);
         }
     }
 }
