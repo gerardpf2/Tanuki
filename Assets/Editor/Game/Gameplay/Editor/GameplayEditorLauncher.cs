ed6cb1fe88702c201f5056805718f171d76b9df9
gerardpf2 <32612924+gerardpf2@users.noreply.github.com>
2025-12-25 19:03:14 +0100

    Added launch from menu support

--- Assets/Editor/Game/Gameplay/Editor/GameplayEditorLauncher.cs
+++ Assets/Editor/Game/Gameplay/Editor/GameplayEditorLauncher.cs
@@ -1,4 +1,6 @@
 using Editor.Game.Gameplay.Editor.UseCases;
+using Infrastructure.System.Exceptions;
+using UnityEditor;
 using UnityEngine;
 
 namespace Editor.Game.Gameplay.Editor
@@ -6,8 +8,42 @@ namespace Editor.Game.Gameplay.Editor
     [CreateAssetMenu(fileName = nameof(GameplayEditorLauncher), menuName = "Tanuki/Editor/Game/Gameplay/" + nameof(GameplayEditorLauncher))]
     public class GameplayEditorLauncher : ScriptableObject
     {
+        [MenuItem("Window/Tanuki/Editor/Game/Gameplay/" + nameof(GameplayEditorLauncher))]
+        public static void LaunchFromMenu()
+        {
+            GameplayEditorLauncher[] gameplayEditorLaunchers = Resources.FindObjectsOfTypeAll<GameplayEditorLauncher>();
+
+            InvalidOperationException.ThrowIfNull(gameplayEditorLaunchers);
+
+            switch (gameplayEditorLaunchers.Length)
+            {
+                case <= 0:
+                {
+                    InvalidOperationException.Throw($"No {nameof(GameplayEditorLauncher)} found");
+
+                    return;
+                }
+                case > 1:
+                {
+                    InvalidOperationException.Throw($"Multiple {nameof(GameplayEditorLauncher)} found");
+
+                    return;
+                }
+                default:
+                {
+                    GameplayEditorLauncher gameplayEditorLauncher = gameplayEditorLaunchers[0];
+
+                    InvalidOperationException.ThrowIfNull(gameplayEditorLauncher);
+
+                    gameplayEditorLauncher.Launch();
+
+                    break;
+                }
+            }
+        }
+
         [ContextMenu(nameof(Launch))]
-        private void Launch()
+        public void Launch()
         {
             // TODO
 
