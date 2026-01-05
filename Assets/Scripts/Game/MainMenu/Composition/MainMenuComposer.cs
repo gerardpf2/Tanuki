f663ab21dd80f9e6c3276acb9d605815aecb809e
gerardpf2 <32612924+gerardpf2@users.noreply.github.com>
2026-01-05 12:20:23 +0100

    Added composer and its builder

---
+++ Assets/Scripts/Game/MainMenu/Composition/MainMenuComposer.cs
@@ -0,0 +1,36 @@
+using Game.MainMenu.UseCases;
+using Infrastructure.DependencyInjection;
+using Infrastructure.ScreenLoading;
+using Infrastructure.System.Exceptions;
+using JetBrains.Annotations;
+
+namespace Game.MainMenu.Composition
+{
+    public class MainMenuComposer : ScopeComposer
+    {
+        protected override void AddRules([NotNull] IRuleAdder ruleAdder, [NotNull] IRuleFactory ruleFactory)
+        {
+            ArgumentNullException.ThrowIfNull(ruleAdder);
+            ArgumentNullException.ThrowIfNull(ruleFactory);
+
+            base.AddRules(ruleAdder, ruleFactory);
+
+            ruleAdder.Add(
+                ruleFactory.GetSingleton<ILoadMainMenuUseCase>(r =>
+                    new LoadMainMenuUseCase(
+                        r.Resolve<IScreenLoader>()
+                    )
+                )
+            );
+        }
+
+        protected override void Initialize([NotNull] IRuleResolver ruleResolver)
+        {
+            ArgumentNullException.ThrowIfNull(ruleResolver);
+
+            base.Initialize(ruleResolver);
+
+            ruleResolver.Resolve<ILoadMainMenuUseCase>().Resolve();
+        }
+    }
+}
