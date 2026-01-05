abe7da64a87107509500548d6b35806b38ce6ba7
gerardpf2 <32612924+gerardpf2@users.noreply.github.com>
2026-01-05 17:04:33 +0100

    Added screen stack

---
+++ Assets/Scripts/Infrastructure/ScreenLoading/ScreenStack.cs
@@ -0,0 +1,67 @@
+using System.Collections.Generic;
+using Infrastructure.System.Exceptions;
+using JetBrains.Annotations;
+
+namespace Infrastructure.ScreenLoading
+{
+    // TODO: Test
+    public class ScreenStack : IScreenStack
+    {
+        [NotNull, ItemNotNull] private readonly LinkedList<IScreen> _screens = new(); // ItemNotNull as long as all Add check for null
+
+        private IScreen FocusedScreen => _screens.Last?.Value;
+
+        public void Push([NotNull] IScreen screen)
+        {
+            ArgumentNullException.ThrowIfNull(screen);
+
+            IScreen focusedScreen = FocusedScreen;
+
+            if (focusedScreen == screen)
+            {
+                return;
+            }
+
+            AddLast(screen);
+
+            focusedScreen?.OnFocus(false);
+            screen.OnFocus(true);
+        }
+
+        public void Remove([NotNull] IScreen screen)
+        {
+            ArgumentNullException.ThrowIfNull(screen);
+
+            IScreen focusedScreen = FocusedScreen;
+
+            if (focusedScreen == screen)
+            {
+                screen.OnFocus(false);
+
+                _screens.RemoveLast();
+            }
+            else if (!TryRemove(screen))
+            {
+                InvalidOperationException.Throw("Cannot remove screen"); // TODO: Add screen name, etc
+            }
+
+            IScreen newFocusedScreen = FocusedScreen;
+
+            newFocusedScreen?.OnFocus(true);
+        }
+
+        private bool TryRemove(IScreen screen)
+        {
+            return _screens.Remove(screen);
+        }
+
+        private void AddLast([NotNull] IScreen screen)
+        {
+            ArgumentNullException.ThrowIfNull(screen);
+
+            TryRemove(screen);
+
+            _screens.AddLast(screen);
+        }
+    }
+}
