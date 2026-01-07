eabcc3e62d23bf56b1bfcedf02e30ec1b6278630
gerardpf2 <32612924+gerardpf2@users.noreply.github.com>
2026-01-07 13:06:27 +0100

    Added tmp button view data

--- Assets/Scripts/Game/Gameplay/View/Header/HeaderViewModel.cs
+++ Assets/Scripts/Game/Gameplay/View/Header/HeaderViewModel.cs
@@ -1,25 +1,40 @@
 using Game.Gameplay.View.Goals;
 using Game.Gameplay.View.Moves;
 using Infrastructure.ModelViewViewModel;
+using Infrastructure.ModelViewViewModel.Examples.Button;
 using JetBrains.Annotations;
 
 namespace Game.Gameplay.View.Header
 {
     public class HeaderViewModel : ViewModel, IDataSettable<HeaderViewData>
     {
+        [NotNull] private readonly IBoundProperty<ButtonViewData> _menuButtonViewData = new BoundProperty<ButtonViewData>("MenuButtonViewData");
         [NotNull] private readonly IBoundProperty<GoalsViewData> _goalsViewData = new BoundProperty<GoalsViewData>("GoalsViewData");
         [NotNull] private readonly IBoundProperty<MovesViewData> _movesViewData = new BoundProperty<MovesViewData>("MovesViewData");
 
         private void Awake()
         {
-            Add(_goalsViewData);
-            Add(_movesViewData);
+            InitializeBindings();
+            AddBindings();
         }
 
         public void SetData(HeaderViewData _)
         {
+            // TODO: Remove if not needed (the same for HeaderViewData, etc)
+        }
+
+        private void InitializeBindings()
+        {
+            _menuButtonViewData.Value = new ButtonViewData(null); // TODO
             _goalsViewData.Value = new GoalsViewData();
             _movesViewData.Value = new MovesViewData();
+        }
+
+        private void AddBindings()
+        {
+            Add(_menuButtonViewData);
+            Add(_goalsViewData);
+            Add(_movesViewData);
         }
     }
 }
